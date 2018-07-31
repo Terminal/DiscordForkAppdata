520cd549e02653ac2f117c9496751b9a36fb5e91
lepon01 <katie@discordmail.com>
2018-07-31 17:04:54 +0100

    allow only bots to reply with JSON

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const client = new Eris.Client(config.token);
 
 // If forkdata doesn't exist, clone
 if (!fs.existsSync(path.join(__dirname, 'forkdata'))) {
-
+  child_process.exec(`git clone ${config.github} -b gh-pages forkdata`);
+  child_process.exec('git config user.name "DiscordForkAppdata"', {
+    cwd: path.join(__dirname, 'forkdata')
+  });
+  child_process.exec('git config user.email "terminal@discordmail.com"', {
+    cwd: path.join(__dirname, 'forkdata')
+  });
 }
 
 client.on('ready', () => {
@@ -17,28 +23,46 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', (message) => {
-  if (message.channel.id !== '353911657665396736') return;
   if (message.author.id === client.user.id) return;
-  try {
-    const inData = JSON.parse(message.content);
-
-    if (typeof inData === 'object') {
-      const outData = {};
-      const botCount = parseInt(inData.botCount) || 0;
-      if (botCount) outData.botCount = botCount;
-
-      const fileData = JSON.stringify(outData);
-
-      fs.writeFile(path.join(__dirname, 'forkdata', `${message.author.id}.json`), fileData, { encoding: 'utf8' }, (error) => {
-        if (error) {
-          message.channel.createMessage(`Error writing to file:\n${error.message}`);
-        } else {
-          message.channel.createMessage(`Successfully wrote to file:\n${fileData}`);
-        }
-      })
+  if (message.author.bot) {
+    try {
+      const inData = JSON.parse(message.content);
+  
+      if (typeof inData === 'object') {
+        const outData = {};
+        const botCount = parseInt(inData.botCount) || 0;
+        if (botCount) outData.botCount = botCount;
+  
+        const fileData = JSON.stringify(outData) + '\n';
+  
+        fs.writeFile(path.join(__dirname, 'forkdata', `${message.author.id}.json`), fileData, { encoding: 'utf8' }, (error) => {
+          if (error) {
+            message.channel.createMessage(`Error writing to file:\n${error.message}`);
+          } else {
+            message.channel.createMessage(`Successfully wrote to file:\n${fileData}`);
+          }
+        })
+      }
+    } catch (e) {
+      console.log(e);
+      // message.channel.createMessage(`Error encountered:\n${e.message}`);
     }
-  } catch (e) {
-    message.channel.createMessage(`Error encountered:\n${e.message}`);
+  } else if (config.owners.includes(message.author.id) && message.content === '£push') {
+    child_process.exec('git add .', {
+      cwd: path.join(__dirname, 'forkdata')
+    });
+    child_process.exec(`git commit -m "Manual push by ${message.author.username.replace(/"/g, '\\"')} via Discord"`, {
+      cwd: path.join(__dirname, 'forkdata')
+    });
+    child_process.exec('git push', {
+      cwd: path.join(__dirname, 'forkdata')
+    }, (error, stdout, stderr) => {
+      if (error) {
+        message.channel.createMessage(`Error running command:\n${error.message}`);
+      } else {
+        message.channel.createMessage(`\`\`\`\nstdout\n${stdout}\n\nstderr\n${stderr}\`\`\``);
+      }
+    });
   }
 });
 
