6430eecb90dd9350e0369503f7943e0d4dbdc9b8
lepon01 <katie@discordmail.com>
2018-11-11 17:42:46 +0000

    fork2ls

--- fork2ls/convert.js
+++ fork2ls/convert.js
@@ -13,27 +13,31 @@ fs.readdir('files', (err, files) => {
       encoding: 'utf8',
     }, (err1, text) => {
       const content = fm(text);
+      // const output = {
+      //   authors: [],
+      //   contents: {
+      //     en: {
+      //       description: content.attributes.description,
+      //       name: content.attributes.pagename,
+      //       page: content.body
+      //     }
+      //   },
+      //   github: {
+      //     owner: content.attributes.github ? content.attributes.github.owner || null : null,
+      //     repo: content.attributes.github ? content.attributes.github.repo || null : null
+      //   },
+      //   id: file.slice(0, -3),
+      //   invite: content.attributes.link,
+      //   oauth: null,
+      //   trigger: {
+      //     customisable: content.attributes.prefix ? content.attributes.prefix.includes('custom') : false,
+      //     mentionable: false,
+      //     prefix: content.attributes.prefix ? [...content.attributes.prefix.split(/ or /g)] : []
+      //   }
+      // }
       const output = {
-        authors: [],
-        contents: {
-          en: {
-            description: content.attributes.description,
-            name: content.attributes.pagename,
-            page: content.body
-          }
-        },
-        github: {
-          owner: content.attributes.github ? content.attributes.github.owner || null : null,
-          repo: content.attributes.github ? content.attributes.github.repo || null : null
-        },
         id: file.slice(0, -3),
-        invite: content.attributes.link,
-        oauth: null,
-        trigger: {
-          customisable: content.attributes.prefix ? content.attributes.prefix.includes('custom') : false,
-          mentionable: false,
-          prefix: content.attributes.prefix ? [...content.attributes.prefix.split(/ or /g)] : []
-        }
+        avatar: content.attributes.avatar
       }
       allFiles.push(output);
       if (index === files.length - 1) done();
